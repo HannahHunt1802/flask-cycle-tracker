d44ad1d0499c6c023e96cf68b0fc380b5b677333
HannahHunt1802 <hannahhunt5656@gmail.com>
2025-11-23 19:58:49 +0000

    feat: developed registration route with basic html file implementation

---
+++ app/routes.py
@@ -0,0 +1,60 @@
+import hashlib
+import traceback
+from flask import request, render_template, redirect, url_for, session, Blueprint, flash, abort, current_app
+from app import db
+from app.forms import RegisterForm
+from app.models import User
+
+main = Blueprint('main', __name__)
+
+#helper method to hash sensitive data for logging
+def hash_for_log(value):
+    return hashlib.sha256(str(value).encode()).hexdigest()
+
+@main.route('/')
+def base():
+    return render_template('base.html')
+
+@main.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    user_ip = request.remote_addr or "Unknown IP" #must be INSIDE route
+
+    if form.validate_on_submit():
+        try:
+            name = form.name.data.strip()
+            email= form.email.data.strip()
+            password = form.password.data.strip()
+
+            new_user = User()
+            new_user.name = name
+            new_user.email = email
+            new_user.set_password(password)
+
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Registration successful!', 'success')
+            current_app.logger.info(f"New registration. Username: {hash_for_log(name)}, IP Address: {hash_for_log(user_ip)}")
+
+            #clear existing session to prevent session fixation
+            session.clear()
+            session['name']= name
+            session['email'] = email
+            return redirect(url_for('main.login'))
+
+        except Exception:
+            db.session.rollback()
+            current_app.logger.error(f"Registration failed, {traceback.format_exc()}, IP: {hash_for_log(user_ip)}")
+            flash("An unexpected error has occurred, please try again.", "error")
+
+    else:
+        if form.errors:
+            for field, errors in form.errors.items():
+                for error in errors:
+                    flash(f"{field}: {error}", 'error')
+                    current_app.logger.warning(f"Registration validation failed. Field: {field}, "
+                                               f"Error: {error}, IP: {hash_for_log(user_ip)}.")
+    return render_template('register.html', form=form)
+
+
+
