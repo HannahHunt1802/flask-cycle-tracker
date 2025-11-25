cbe3f752131a250a1b89bb382b39228a1f91b8b4
HannahHunt1802 <hannahhunt5656@gmail.com>
2025-11-25 18:21:16 +0000

    fix: created event listener to add default/non-nullable values in CycleSettings table whenever a new user is created

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from app import db, bcrypt
-from sqlalchemy import DateTime, func
+from sqlalchemy import DateTime, func, event
 from flask_login import UserMixin
 
 # user table
@@ -30,11 +30,23 @@ class CycleSettings(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-    regular_cycle = db.Column(db.Boolean, nullable=True, default=True)  # true if regular, false if irregular
-    avg_period_length = db.Column(db.Integer, nullable=True, default=5)
-    avg_cycle_length = db.Column(db.Integer, nullable=True, default=28)
+    regular_cycle = db.Column(db.Boolean, nullable=False, default=True)  # true if regular, false if irregular
+    avg_period_length = db.Column(db.Integer, nullable=False, default=5)
+    avg_cycle_length = db.Column(db.Integer, nullable=False, default=28)
 
     updated_at = db.Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+
+#event listener to automatically insert default cycle settings when a new user is created
+@event.listens_for(User, "after_insert")
+def create_default_related_rows(mapper, connection, target):
+    connection.execute(
+        CycleSettings.__table__.insert().values(
+            user_id=target.id,
+            regular_cycle=True,
+            avg_period_length=5,
+            avg_cycle_length=28
+        )
+    )
 
 #individual period table
 class PeriodLog(db.Model):
